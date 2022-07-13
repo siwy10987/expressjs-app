ef536e0cd4c4d421d560aae6d1c4c0a33b65e19e
unknown <wykalaczka8@gmail.com>
2022-07-13 21:50:13 +0200

    Dodano gulpa2

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
-gulp.task('buildjs', function(){
+gulp.task('buildjs', async function(){
 	gulp.src('source/*.js')
 	.pipe(uglify())
 	.pipe(gulp.dest('dist'));
